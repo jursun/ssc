e47396601a35375826d4ffdf68c98f727224cedb
Jason Wong <jursun@gmail.com>
2011-03-17 22:07:13 -1000

    Added:  - Basic CSS template  - Helper files to the application.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
   def home
+    @title = "News"
   end
 
   def contact
+    @title = "Contacts"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  #Generate the title
+  def title
+    base_title = "Survival Starcraft"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def banner
+    image_tag("rails.png", :alt => "Survival Starcraft", :class => "round")
+  end
+
 end
