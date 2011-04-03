333950b45dd23d6d05672ca17609a01431c51c88
Jason Wong <jursun@gmail.com>
2011-04-02 23:57:17 -1000

    - Added in place holders for pages  - Added new navigation bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,19 @@ class PagesController < ApplicationController
     @title = "News"
   end
 
+  def rules
+    @title = "Rules"
+  end
+
   def contact
     @title = "Contacts"
+  end
+
+  def terms
+    @title = "Terms of Service"
+  end
+
+  def policy
+    @title = "Private Policy"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ SurvivalStarcraft::Application.routes.draw do
   devise_for :users
   resources :users
 
+  match '/users', :to => 'users#index'
+
+  match '/rules', :to => 'pages#rules'
   match '/contact', :to => 'pages#contact'
+  match '/terms', :to => 'pages#terms'
+  match '/policy', :to => 'pages#policy'
 
   root :to => "pages#home"
 end
